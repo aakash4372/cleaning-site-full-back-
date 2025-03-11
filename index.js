fbbbb6ae169a42a89c69432505d24d32a5c8cb29
aakash4372 <aakashaakash0013@gmail.com>
2025-03-11 13:04:10 +0530

    code

--- index.js
+++ index.js
@@ -8,10 +8,18 @@ const PORT = process.env.PORT || 5000;
 
 // Enable CORS for frontend
 app.use(cors({
-    origin: "https://clening-site-front.vercel.app",
-    methods: "GET,POST",
-    allowedHeaders: "Content-Type"
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
 }));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 
 app.use(express.json()); // Middleware to parse JSON
 
